94c98feebecd91d6c4807073a97bd4acca99a554
kevin_yang <kevin_yang@comwave.com.tw>
2024-05-08 10:54:09 +0800

    first commit

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ def get_contacts():
     json_contacts = list(map(lambda x: x.to_json(), contacts))
     return jsonify({"contacts": json_contacts})
 
-@app.route("/create_contact",methods={"POST"})
+@app.route("/create_contact",methods=["POST"])
 def create_contact():
     first_name = request.json.get("firstName")
     last_name = request.json.get("lastName")
